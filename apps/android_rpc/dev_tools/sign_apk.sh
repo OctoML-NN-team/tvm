ff35fc5c83ef136a65d3115d5f3502451c1c11f8
Alexander Peskov <apeskov@Alexanders-MacBook-Pro.local>
2021-02-17 14:23:47 +0300

    Build android-rpc with latest tools
    Signed-off-by: Alexander Peskov <apeskov@Alexanders-MacBook-Pro.local>

--- apps/android_rpc/dev_tools/sign_apk.sh
+++ apps/android_rpc/dev_tools/sign_apk.sh
@@ -19,5 +19,5 @@ CURR_DIR=$(cd `dirname $0`; pwd)
 APK_DIR=$CURR_DIR/../app/build/outputs/apk/release
 UNSIGNED_APK=$APK_DIR/app-release-unsigned.apk
 SIGNED_APK=$APK_DIR/tvmrpc-release.apk
-jarsigner -verbose -keystore $CURR_DIR/tvmrpc.keystore -signedjar $SIGNED_APK $UNSIGNED_APK 'tvmrpc'
+apksigner sign --ks $CURR_DIR/tvmrpc.keystore --out $SIGNED_APK --in $UNSIGNED_APK
 echo $SIGNED_APK
