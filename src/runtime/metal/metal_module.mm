e3d99c1e353f70e13aa6e6fc0a06f00376d4ede4
Egor Churaev <egor.churaev@gmail.com>
2021-03-03 18:35:42 +0300

    [METAL] Fix deferred memory release in Metal runtime
    In case when we build runtime without ARC, we can have problems with
    memory releasing. Due to some of Objective-C methods returns
    autoreleased pointers, we should specify `autoreleasepool` blocks to
    determine life cycle of these pointers.
    
    Without this fix I had have a problem that memory was over when a
    topology was executed multiple times.

--- src/runtime/metal/metal_module.mm
+++ src/runtime/metal/metal_module.mm
@@ -85,50 +85,56 @@ class MetalModuleNode final : public runtime::ModuleNode {
     if (it != e.smap.end()) return it->second;
     // compile
     NSError* err_msg = nil;
-    if (e.lib == nil) {
-      if (fmt_ == "metal") {
-        MTLCompileOptions* opts = [MTLCompileOptions alloc];
-        opts.languageVersion = MTLLanguageVersion2_3;
-        opts.fastMathEnabled = YES;
-        // opts = nil;
-        e.lib = [w->devices[device_id]
-            newLibraryWithSource:[NSString stringWithUTF8String:data_.c_str()]
-                         options:opts
-                           error:&err_msg];
-        [opts dealloc];
-        if (e.lib == nil) {
-          LOG(FATAL) << "Fail to compile metal lib:" << [[err_msg localizedDescription] UTF8String];
+    @autoreleasepool {
+      if (e.lib == nil) {
+        if (fmt_ == "metal") {
+          MTLCompileOptions* opts = [MTLCompileOptions alloc];
+          opts.languageVersion = MTLLanguageVersion2_3;
+          opts.fastMathEnabled = YES;
+          // opts = nil;
+          e.lib = [w->devices[device_id]
+              newLibraryWithSource:[NSString stringWithUTF8String:data_.c_str()]
+                           options:opts
+                             error:&err_msg];
+          [opts dealloc];
+          if (e.lib == nil) {
+            LOG(FATAL) << "Fail to compile metal lib:"
+                       << [[err_msg localizedDescription] UTF8String];
+          }
+          if (err_msg != nil) {
+            LOG(INFO) << "Warning: " << [[err_msg localizedDescription] UTF8String];
+          }
+        } else {
+          // Build from library.
+          auto q = dispatch_queue_create("q", DISPATCH_QUEUE_SERIAL);
+          auto data = dispatch_data_create(data_.c_str(), data_.length(), q,
+                                           ^{
+                                           });
+          e.lib = [w->devices[device_id] newLibraryWithData:data error:&err_msg];
+          if (err_msg != nil || e.lib == nil) {
+            LOG(FATAL) << "Fail to compile metal lib:"
+                       << [[err_msg localizedDescription] UTF8String];
+          }
         }
-        if (err_msg != nil) {
-          LOG(INFO) << "Warning: " << [[err_msg localizedDescription] UTF8String];
-        }
-      } else {
-        // Build from library.
-        auto q = dispatch_queue_create("q", DISPATCH_QUEUE_SERIAL);
-        auto data = dispatch_data_create(data_.c_str(), data_.length(), q,
-                                         ^{
-                                         });
-        e.lib = [w->devices[device_id] newLibraryWithData:data error:&err_msg];
-        if (err_msg != nil || e.lib == nil) {
-          LOG(FATAL) << "Fail to compile metal lib:" << [[err_msg localizedDescription] UTF8String];
-        }
+        [e.lib retain];
       }
-      [e.lib retain];
+      id<MTLFunction> f =
+          [e.lib newFunctionWithName:[NSString stringWithUTF8String:func_name.c_str()]];
+      ICHECK(f != nil) << "cannot find function " << func_name;
+      id<MTLComputePipelineState> state =
+          [w->devices[device_id] newComputePipelineStateWithFunction:f error:&err_msg];
+      ICHECK(state != nil) << "cannot get state:"
+                           << " for function " << func_name
+                           << [[err_msg localizedDescription] UTF8String];
+      [f release];
+      // The state.threadExecutionWidth can change dynamically according
+      // to the resource constraint in kernel, so it is not strictly hold
+      // Turn of warp aware optimziation for now.
+      // ICHECK_EQ(state.threadExecutionWidth, w->warp_size[device_id]);
+      if (e.smap[func_name] != nil) [e.smap[func_name] release];
+      e.smap[func_name] = [state retain];
+      return state;
     }
-    id<MTLFunction> f =
-        [e.lib newFunctionWithName:[NSString stringWithUTF8String:func_name.c_str()]];
-    ICHECK(f != nil) << "cannot find function " << func_name;
-    id<MTLComputePipelineState> state =
-        [w->devices[device_id] newComputePipelineStateWithFunction:f error:&err_msg];
-    ICHECK(state != nil) << "cannot get state:"
-                         << " for function " << func_name
-                         << [[err_msg localizedDescription] UTF8String];
-    // The state.threadExecutionWidth can change dynamically according
-    // to the resource constraint in kernel, so it is not strictly hold
-    // Turn of warp aware optimziation for now.
-    // ICHECK_EQ(state.threadExecutionWidth, w->warp_size[device_id]);
-    e.smap[func_name] = [state retain];
-    return state;
   }
 
  private:
@@ -188,24 +194,26 @@ class MetalWrappedFunc {
     }
     ThreadWorkLoad wl = thread_axis_cfg_.Extract(args);
     id<MTLCommandQueue> queue = w_->GetCommandQueue(t->context);
-    id<MTLCommandBuffer> cb = [queue commandBuffer];
-    id<MTLComputeCommandEncoder> encoder = [cb computeCommandEncoder];
-    [encoder setComputePipelineState:scache_[device_id]];
-    for (size_t i = 0; i < num_buffer_args_; ++i) {
-      void* buf = args[static_cast<int>(i)];
-      [encoder setBuffer:(__bridge id<MTLBuffer>)(buf) offset:0 atIndex:i];
+    @autoreleasepool {
+      id<MTLCommandBuffer> cb = [queue commandBuffer];
+      id<MTLComputeCommandEncoder> encoder = [cb computeCommandEncoder];
+      [encoder setComputePipelineState:scache_[device_id]];
+      for (size_t i = 0; i < num_buffer_args_; ++i) {
+        void* buf = args[static_cast<int>(i)];
+        [encoder setBuffer:(__bridge id<MTLBuffer>)(buf) offset:0 atIndex:i];
+      }
+      if (num_pack_args_ != 0) {
+        [encoder setBytes:pack_args
+                   length:num_pack_args_ * sizeof(ArgUnion)
+                  atIndex:num_buffer_args_];
+      }
+      // launch
+      MTLSize dimGrid = MTLSizeMake(wl.grid_dim(0), wl.grid_dim(1), wl.grid_dim(2));
+      MTLSize dimBlock = MTLSizeMake(wl.block_dim(0), wl.block_dim(1), wl.block_dim(2));
+      [encoder dispatchThreadgroups:dimGrid threadsPerThreadgroup:dimBlock];
+      [encoder endEncoding];
+      [cb commit];
     }
-    if (num_pack_args_ != 0) {
-      [encoder setBytes:pack_args
-                 length:num_pack_args_ * sizeof(ArgUnion)
-                atIndex:num_buffer_args_];
-    }
-    // launch
-    MTLSize dimGrid = MTLSizeMake(wl.grid_dim(0), wl.grid_dim(1), wl.grid_dim(2));
-    MTLSize dimBlock = MTLSizeMake(wl.block_dim(0), wl.block_dim(1), wl.block_dim(2));
-    [encoder dispatchThreadgroups:dimGrid threadsPerThreadgroup:dimBlock];
-    [encoder endEncoding];
-    [cb commit];
   }
 
  private:
