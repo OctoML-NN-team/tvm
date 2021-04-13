ef35e058df1a12640f0047ad7b9f2019e55f3294
KJlaccHoeUM9l <wotpricol@mail.ru>
2021-04-13 17:00:59 +0300

    test GH actions for onnx models

--- tests/python/contrib/test_bnns/test_onnx_topologies.py
+++ tests/python/contrib/test_bnns/test_onnx_topologies.py
@@ -19,6 +19,7 @@
 import pytest
 
 import tvm
+import onnx
 from tvm import relay
 from tvm.relay import transform
 from tvm.contrib import utils, graph_executor
@@ -36,13 +37,13 @@ INPUT_SHAPE = [1, 3, 224, 224]
 
 BASE_MODEL_URL = "https://github.com/onnx/models/raw/master/"
 MODEL_URL_COLLECTION = {
-    "BERT": "text/machine_comprehension/bert-squad/model/bertsquad-10.onnx",
+    # "BERT": "text/machine_comprehension/bert-squad/model/bertsquad-10.onnx",
     "MobileNet-v2": "vision/classification/mobilenet/model/mobilenetv2-7.onnx",
     "ResNet50-v1": "vision/classification/resnet/model/resnet50-v1-7.onnx",
     "ResNet50-v2": "vision/classification/resnet/model/resnet50-v2-7.onnx",
-    "SqueezeNet-v1.1": "vision/classification/squeezenet/model/squeezenet1.1-7.onnx",
-    "SqueezeNet-v1.0": "vision/classification/squeezenet/model/squeezenet1.0-7.onnx",
-    "Inception-v1": "vision/classification/inception_and_googlenet/inception_v1/model/inception-v1-7.onnx",
+    # "SqueezeNet-v1.1": "vision/classification/squeezenet/model/squeezenet1.1-7.onnx",
+    # "SqueezeNet-v1.0": "vision/classification/squeezenet/model/squeezenet1.0-7.onnx",
+    # "Inception-v1": "vision/classification/inception_and_googlenet/inception_v1/model/inception-v1-7.onnx",
     "Inception-v2": "vision/classification/inception_and_googlenet/inception_v2/model/inception-v2-7.onnx",
 }
 
@@ -133,8 +134,17 @@ def process(model_name):
     )
 
 
-@pytest.mark.skip(reason="Manually disabled because of huge complexity")
+# @pytest.mark.skip(reason="Manually disabled because of huge complexity")
 @pytest.mark.skipif(bnns_is_absent, reason="BNNS runtime is absent")
 @pytest.mark.parametrize("model_name", MODEL_URL_COLLECTION.keys())
 def test_topology(model_name):
     process(model_name)
+
+
+def main():
+    for model_name in MODEL_URL_COLLECTION.keys():
+        test_topology(model_name)
+
+
+if __name__ == "__main__":
+    main()
