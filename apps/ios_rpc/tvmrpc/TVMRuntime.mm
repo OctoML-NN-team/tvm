dc7ae2f58234f977a25d10909f6763ddabd183ca
Alexander Peskov <peskovnn@gmail.com>
2021-04-16 12:29:16 +0300

    [iOS] Add tracker support into ios-rpc app
    Also containes:
    * Minor improvements from UX perspective
    * Changes in build process. Use tvm_runtime as external module
    
    Signed-off-by: Alexander Peskov <peskovnn@gmail.com>

--- apps/ios_rpc/tvmrpc/TVMRuntime.mm
+++ apps/ios_rpc/tvmrpc/TVMRuntime.mm
@@ -22,36 +22,38 @@
  */
 #include "TVMRuntime.h"
 // Runtime API
-#include "../../../src/runtime/c_runtime_api.cc"
-#include "../../../src/runtime/cpu_device_api.cc"
-#include "../../../src/runtime/dso_library.cc"
-#include "../../../src/runtime/file_utils.cc"
-#include "../../../src/runtime/library_module.cc"
-#include "../../../src/runtime/metadata_module.cc"
-#include "../../../src/runtime/module.cc"
-#include "../../../src/runtime/ndarray.cc"
-#include "../../../src/runtime/object.cc"
-#include "../../../src/runtime/registry.cc"
-#include "../../../src/runtime/system_library.cc"
-#include "../../../src/runtime/thread_pool.cc"
-#include "../../../src/runtime/threading_backend.cc"
-#include "../../../src/runtime/workspace_pool.cc"
-
-// RPC server
-#include "../../../src/runtime/rpc/rpc_channel.cc"
-#include "../../../src/runtime/rpc/rpc_endpoint.cc"
-#include "../../../src/runtime/rpc/rpc_local_session.cc"
-#include "../../../src/runtime/rpc/rpc_module.cc"
-#include "../../../src/runtime/rpc/rpc_server_env.cc"
-#include "../../../src/runtime/rpc/rpc_session.cc"
-#include "../../../src/runtime/rpc/rpc_socket_impl.cc"
-// Graph executor
-#include "../../../src/runtime/graph_executor/graph_executor.cc"
-// Metal
-#include "../../../src/runtime/metal/metal_device_api.mm"
-#include "../../../src/runtime/metal/metal_module.mm"
-// CoreML
-#include "../../../src/runtime/contrib/coreml/coreml_runtime.mm"
+//#include "../../../src/runtime/c_runtime_api.cc"
+//#include "../../../src/runtime/cpu_device_api.cc"
+//#include "../../../src/runtime/dso_library.cc"
+//#include "../../../src/runtime/file_utils.cc"
+//#include "../../../src/runtime/library_module.cc"
+//#include "../../../src/runtime/metadata_module.cc"
+//#include "../../../src/runtime/module.cc"
+//#include "../../../src/runtime/ndarray.cc"
+//#include "../../../src/runtime/object.cc"
+//#include "../../../src/runtime/registry.cc"
+//#include "../../../src/runtime/system_library.cc"
+//#include "../../../src/runtime/thread_pool.cc"
+//#include "../../../src/runtime/threading_backend.cc"
+//#include "../../../src/runtime/workspace_pool.cc"
+//#include "../../../src/runtime/profiling.cc"
+//#include "../../../src/runtime/logging.cc"
+//
+//// RPC server
+//#include "../../../src/runtime/rpc/rpc_channel.cc"
+//#include "../../../src/runtime/rpc/rpc_endpoint.cc"
+//#include "../../../src/runtime/rpc/rpc_local_session.cc"
+//#include "../../../src/runtime/rpc/rpc_module.cc"
+//#include "../../../src/runtime/rpc/rpc_server_env.cc"
+//#include "../../../src/runtime/rpc/rpc_session.cc"
+//#include "../../../src/runtime/rpc/rpc_socket_impl.cc"
+//// Graph executor
+//#include "../../../src/runtime/graph_executor/graph_executor.cc"
+//// Metal
+//#include "../../../src/runtime/metal/metal_device_api.mm"
+//#include "../../../src/runtime/metal/metal_module.mm"
+//// CoreML
+//#include "../../../src/runtime/contrib/coreml/coreml_runtime.mm"
 
 namespace tvm {
 namespace runtime {
@@ -71,88 +73,19 @@ void LogMessageImpl(const std::string& file, int lineno, const std::string& mess
 namespace tvm {
 namespace runtime {
 
-class NSStreamChannel final : public RPCChannel {
- public:
-  explicit NSStreamChannel(NSOutputStream* stream) : stream_(stream) {}
-
-  size_t Send(const void* data, size_t size) final {
-    ssize_t nbytes = [stream_ write:reinterpret_cast<const uint8_t*>(data) maxLength:size];
-    if (nbytes < 0) {
-      NSLog(@"%@", [stream_ streamError].localizedDescription);
-      throw tvm::Error("Stream error");
-    }
-    return nbytes;
-  }
-
-  size_t Recv(void* data, size_t size) final {
-    LOG(FATAL) << "Do not allow explicit receive for";
-    return 0;
-  }
-
- private:
-  NSOutputStream* stream_;
-};
-
-FEventHandler CreateServerEventHandler(NSOutputStream* outputStream, std::string name,
-                                       std::string remote_key) {
-  std::unique_ptr<NSStreamChannel> ch(new NSStreamChannel(outputStream));
-  std::shared_ptr<RPCEndpoint> sess = RPCEndpoint::Create(std::move(ch), name, remote_key);
-  return [sess](const std::string& in_bytes, int flag) {
-    return sess->ServerAsyncIOEventHandler(in_bytes, flag);
-  };
-}
-
-// Runtime environment
-struct RPCEnv {
- public:
-  RPCEnv() {
-    NSString* path = NSTemporaryDirectory();
-    base_ = [path UTF8String];
-    if (base_[base_.length() - 1] != '/') {
-      base_ = base_ + '/';
-    }
-  }
-  // Get Path.
-  std::string GetPath(const std::string& file_name) { return base_ + file_name; }
-
- private:
-  std::string base_;
-};
-
-void LaunchSyncServer() {
-  // only load dylib from frameworks.
-  NSBundle* bundle = [NSBundle mainBundle];
-  NSString* base = [bundle privateFrameworksPath];
-  NSString* path = [base stringByAppendingPathComponent:@"tvm/rpc_config.txt"];
-  std::string name = [path UTF8String];
-  std::ifstream fs(name, std::ios::in);
-  std::string url, key;
-  int port;
-  ICHECK(fs >> url >> port >> key) << "Invalid RPC config file " << name;
-  RPCConnect(url, port, "server:" + key, TVMArgs(nullptr, nullptr, 0))->ServerLoop();
-}
-
 TVM_REGISTER_GLOBAL("tvm.rpc.server.workpath").set_body([](TVMArgs args, TVMRetValue* rv) {
-  static RPCEnv env;
-  *rv = env.GetPath(args[0]);
+  std::string name = args[0];
+  std::string base = [NSTemporaryDirectory() UTF8String];
+  *rv = base + "/" + name;
 });
 
 TVM_REGISTER_GLOBAL("tvm.rpc.server.load_module").set_body([](TVMArgs args, TVMRetValue* rv) {
   std::string name = args[0];
-  std::string fmt = GetFileFormat(name, "");
-  NSString* base;
-  if (fmt == "dylib") {
-    // only load dylib from frameworks.
-    NSBundle* bundle = [NSBundle mainBundle];
-    base = [[bundle privateFrameworksPath] stringByAppendingPathComponent:@"tvm"];
-  } else {
-    // Load other modules in tempdir.
-    base = NSTemporaryDirectory();
-  }
+  NSString* base = NSTemporaryDirectory();
   NSString* path =
       [base stringByAppendingPathComponent:[NSString stringWithUTF8String:name.c_str()]];
   name = [path UTF8String];
-  *rv = Module::LoadFromFile(name, fmt);
+  *rv = Module::LoadFromFile(name);
   LOG(INFO) << "Load module from " << name << " ...";
 });
 }  // namespace runtime
@@ -161,7 +94,8 @@ TVM_REGISTER_GLOBAL("tvm.rpc.server.load_module").set_body([](TVMArgs args, TVMR
 @implementation TVMRuntime
 
 + (void)launchSyncServer {
-  tvm::runtime::LaunchSyncServer();
+  throw "UNIMPLEMENTED";
+//  tvm::runtime::LaunchSyncServer();
 }
 
 @end
--- apps/ios_rpc/tvmrpc/ViewController.mm
+++ apps/ios_rpc/tvmrpc/ViewController.mm
@@ -22,159 +22,85 @@
  */
 
 #import "ViewController.h"
+
+#include "rpc_server.h"
 #include <string>
 
 @implementation ViewController
-
-- (void)stream:(NSStream*)strm handleEvent:(NSStreamEvent)event {
-  std::string buffer;
-  switch (event) {
-    case NSStreamEventOpenCompleted: {
-      self.statusLabel.text = @"Connected";
-      break;
-    }
-    case NSStreamEventHasBytesAvailable:
-      if (strm == inputStream_) {
-        [self onReadAvailable];
-      }
-      break;
-    case NSStreamEventHasSpaceAvailable: {
-      if (strm == outputStream_) {
-        [self onWriteAvailable];
-      }
-      break;
-    }
-    case NSStreamEventErrorOccurred: {
-      NSLog(@"%@", [strm streamError].localizedDescription);
-      break;
-    }
-    case NSStreamEventEndEncountered: {
-      [self close];
-      // auto reconnect when normal end.
-      [self open];
-      break;
-    }
-    default: {
-      NSLog(@"Unknown event");
-    }
-  }
-}
-
-- (void)onReadAvailable {
-  constexpr int kRPCMagic = 0xff271;
-  if (!initialized_) {
-    int code;
-    size_t nbytes = [inputStream_ read:reinterpret_cast<uint8_t*>(&code) maxLength:sizeof(code)];
-    if (nbytes != sizeof(code)) {
-      self.infoText.text = @"Fail to receive remote confirmation code.";
-      [self close];
-    } else if (code == kRPCMagic + 2) {
-      self.infoText.text = @"Proxy server cannot find client that matches the key";
-      [self close];
-    } else if (code == kRPCMagic + 1) {
-      self.infoText.text = @"Proxy server already have another server with same key";
-      [self close];
-    } else if (code != kRPCMagic) {
-      self.infoText.text = @"Given address is not a TVM RPC Proxy";
-      [self close];
-    } else {
-      initialized_ = true;
-      self.statusLabel.text = @"Proxy connected.";
-      ICHECK(handler_ != nullptr);
-    }
-  }
-  const int kBufferSize = 4 << 10;
-  if (initialized_) {
-    while ([inputStream_ hasBytesAvailable]) {
-      recvBuffer_.resize(kBufferSize);
-      uint8_t* bptr = reinterpret_cast<uint8_t*>(&recvBuffer_[0]);
-      size_t nbytes = [inputStream_ read:bptr maxLength:kBufferSize];
-      recvBuffer_.resize(nbytes);
-      int flag = 1;
-      if ([outputStream_ hasSpaceAvailable]) {
-        flag |= 2;
-      }
-      // always try to write
-      try {
-        flag = handler_(recvBuffer_, flag);
-        if (flag == 2) {
-          [self onShutdownReceived];
-        }
-      } catch (const tvm::Error& e) {
-        [self close];
-      }
-    }
-  }
-}
 
 - (void)onShutdownReceived {
   [self close];
 }
 
-- (void)onWriteAvailable {
-  if (initSendPtr_ < initBytes_.length()) {
-    initSendPtr_ += [outputStream_ write:reinterpret_cast<uint8_t*>(&initBytes_[initSendPtr_])
-                               maxLength:(initBytes_.length() - initSendPtr_)];
-  }
-  if (initialized_) {
-    try {
-      std::string dummy;
-      int flag = handler_(dummy, 2);
-      if (flag == 2) {
-        [self onShutdownReceived];
-      }
-    } catch (const tvm::Error& e) {
-      [self close];
-    }
-  }
+- (void)viewDidLoad {
+  self.proxyURL.delegate = self;
+  self.proxyPort.delegate = self;
+  self.proxyKey.delegate = self;
+  
+  [self readPreferences];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+  [self updatePreferences];
+  [[self view] endEditing:YES];
+  return FALSE;
+}
+
+- (void)readPreferences {
+  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+  
+  self.proxyURL.text = [defaults stringForKey:@"tmvrpc_url"];
+  self.proxyPort.text = [defaults stringForKey:@"tmvrpc_port"];
+  self.proxyKey.text = [defaults stringForKey:@"tmvrpc_key"];
+}
+
+- (void)updatePreferences {
+  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+  [defaults setObject:self.proxyURL.text forKey:@"tmvrpc_url"];
+  [defaults setObject:self.proxyPort.text forKey:@"tmvrpc_port"];
+  [defaults setObject:self.proxyKey.text forKey:@"tmvrpc_key"];
 }
 
 - (void)open {
-  constexpr int kRPCMagic = 0xff271;
-  NSLog(@"Connecting to the proxy server..");
-  // Initialize the data states.
-  key_ = [self.proxyKey.text UTF8String];
-  key_ = "server:" + key_;
-  std::ostringstream os;
-  int rpc_magic = kRPCMagic;
-  os.write(reinterpret_cast<char*>(&rpc_magic), sizeof(rpc_magic));
-  int keylen = static_cast<int>(key_.length());
-  os.write(reinterpret_cast<char*>(&keylen), sizeof(keylen));
-  os.write(key_.c_str(), key_.length());
-  initialized_ = false;
-  initBytes_ = os.str();
-  initSendPtr_ = 0;
-  // Initialize the network.
-  CFReadStreamRef readStream;
-  CFWriteStreamRef writeStream;
-  CFStreamCreatePairWithSocketToHost(NULL, (__bridge CFStringRef)self.proxyURL.text,
-                                     [self.proxyPort.text intValue], &readStream, &writeStream);
-  inputStream_ = (__bridge_transfer NSInputStream*)readStream;
-  outputStream_ = (__bridge_transfer NSOutputStream*)writeStream;
-  [inputStream_ setDelegate:self];
-  [outputStream_ setDelegate:self];
-  [inputStream_ scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-  [outputStream_ scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-  [outputStream_ open];
-  [inputStream_ open];
-  handler_ = tvm::runtime::CreateServerEventHandler(outputStream_, key_, "%toinit");
-  ICHECK(handler_ != nullptr);
+  NSLog(@"Connecting to the tracker server..");
+  
+  std::string key_ = [self.proxyKey.text UTF8String];
+  std::string url_ = [self.proxyURL.text UTF8String];
+  int port_ = [self.proxyPort.text intValue];
+
+  std::ostringstream ss;
+  ss << "('" << url_ << "', " << port_<< ")";
+  
+  std::string host = "0.0.0.0";
+  int port = 9000;
+  int port_end = 9099;
+  std::string tracker_addr = ss.str();
+  std::string custom_addr = "";
+  std::string work_dir = [NSTemporaryDirectory() UTF8String];
+  
+  // Start the rpc server
+  rpc_ = std::make_shared<tvm::runtime::RPCServer>(host, port, port_end, tracker_addr, key_, custom_addr, work_dir);
+  rpc_->setCompletionCallbacks(
+    [self] () {
+      dispatch_sync(dispatch_get_main_queue(), ^{
+        self.statusLabel.text = @"Connected";
+      });
+    }, [self] () {
+      dispatch_sync(dispatch_get_main_queue(), ^{
+        self.statusLabel.text = @"Disconnected";
+      });
+    });
+  rpc_->Start();
+
   self.infoText.text = @"";
   self.statusLabel.text = @"Connecting...";
 }
 
+
 - (void)close {
   NSLog(@"Closing the streams.");
-  [inputStream_ close];
-  [outputStream_ close];
-  [inputStream_ removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-  [outputStream_ removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-  [inputStream_ setDelegate:nil];
-  [outputStream_ setDelegate:nil];
-  inputStream_ = nil;
-  outputStream_ = nil;
-  handler_ = nullptr;
-  self.statusLabel.text = @"Disconnected";
+  rpc_->Stop();
 }
 
 - (IBAction)connect:(id)sender {
